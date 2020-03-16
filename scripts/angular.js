fa6ecf827908fa34080f60e8dc2c1aa3b6e02841
Narayan Pandey <npandey722@gmail.com>
2020-03-16 13:23:01 +0530

    initial commit Ishop project

---
+++ scripts/angular.js
@@ -0,0 +1,84 @@
+var app = angular.module("IShop", ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: 'home.html'
+
+        })
+        .when("/products", {
+            templateUrl: "products.html",
+        })
+        .when("/login", {
+            templateUrl: "login.html",
+            controller: "LoginController"
+        })
+        .when("/register", {
+            templateUrl: "register.html",
+            controller: "RegisterController"
+        })
+        .when("/home", {
+            templateUrl: "home.html"
+        })
+})
+app.controller("RegisterController", function ($scope, $http) {
+    $scope.SubmitClick = function (user) {
+        $http.post("http://127.0.0.1:8080/RegisterUser", { UserName: user.UserName, Password: user.Password, Email: user.Email, City: user.City, State: user.State, PinCode: user.PinCode })
+        location.href = "login.html";
+    }
+})
+app.controller("LoginController", function ($scope, $http) {
+    $scope.userDetails = [];
+    $http.get("http://127.0.0.1:8080/userDetails")
+        .then(function (response) {
+            $scope.userDetails = response.data;
+        })
+    $scope.Login = function () {
+        $scope.txtName = "";
+        $scope.txtPwd = "";
+        alert($scope.txtName);
+        for (var i = 0; i < $scope.userDetails.length; i++) {
+            if ($scope.userDetails[i].UserName == $scope.txtName && $scope.userDetails[i].Password == $scope.txtPwd) {
+                location.href = "index.html";
+                break;
+            } else {
+                location.href = "error.html";
+            }
+        }
+    }
+})
+app.controller("HomeController", function ($scope, $http) {
+    $scope.products = [];
+    $scope.categories = [];
+    $http.get("http://127.0.0.1:8080/ProductDetails")
+        .then(function (response) {
+            $scope.products = response.data;
+        })
+    $http.get("http://127.0.0.1:8080/CategoryDetails")
+        .then(function (response) {
+            $scope.categories = response.data;
+        })
+    $scope.selectedCategory = 0;
+    $scope.GetCategory = function (obj) {
+        $scope.selectedCategory = obj.categoryId;
+    }
+    $scope.AddCounter = function (item) { item.quantity++; };
+    $scope.MinusCounter = function (item) { item.quantity--; };
+    $scope.cartItems = [];
+    $scope.AddToCart = function (item) {
+        $scope.cartItems.push(item);
+    }
+    $scope.Delete = function (index) {
+        var verify = confirm("Are you sure , want to Delete?");
+        if (verify == true) {
+            $scope.cartItems.splice(index, 1);
+        }
+    }
+
+})
+app.controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+    function ($scope, $location, $anchorScroll) {
+        $scope.goToTOP = function () {
+            $location.hash('top');
+            $anchorScroll();
+        };
+    }]);
