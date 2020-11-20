cc5cb662918da488675f81e0a3ca15945b23ec46
Narayan Pandey <npandey722@gmail.com>
2020-11-20 11:47:22 +0530

    Ishop

--- BusinessLayer.js
+++ BusinessLayer.js
@@ -4,41 +4,55 @@ var bodyParser = require("body-parser");
 
 var url = "mongodb://127.0.0.1:27017";
 var app = express();
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(bodyParser.json());
 app.use(express.static("Images"));
 
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
 
-    res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-    res.header("Access-Control-Allow-Methods", "GET", "POST", "PUT");
-    next();
-
+  res.header("Access-Control-Allow-Methods", "GET", "POST", "PUT");
+  next();
 });
-
 
 //API Requests
 
 app.post("/RegisterUser", function (req, res) {
-    mongoClient.connect(url, function (err, db) {
-        if (!err) {
-            var dbo = db.db("ishopdb");
-            dbo.collection("tblregister").insertOne({ UserName: req.body.UserName, Password: req.body.Password, Email: req.body.Email, City: req.body.City, State: req.body.State, PinCode: req.body.PinCode }, function (err, result) {
-                if (!err) {
-                    console.log("Registered Successfully..");
-                } else {
-                    console.log(err);
-                }
-            });
-        }
-    })
-})
+  mongoClient.connect(url, function (err, db) {
+    if (!err) {
+      var dbo = db.db("ishopdb");
+      dbo
+        .collection("tblregister")
+        .insertOne(
+          {
+            UserName: req.body.UserName,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            City: req.body.City,
+            State: req.body.State,
+            PinCode: req.body.PinCode,
+          },
+          function (err, result) {
+            if (!err) {
+              console.log("Registered Successfully..");
+            } else {
+              console.log(err);
+            }
+          }
+        );
+    }
+  });
+});
 /*app.post("/UserLogin", function (req, res) {
     mongoClient.connect(url, function (err, db) {
         if (!err) {
@@ -54,44 +68,51 @@ app.post("/RegisterUser", function (req, res) {
     })
 });*/
 
-
 app.get("/userDetails", function (req, res) {
-    mongoClient.connect(url, function (err, db) {
-        var dbo = db.db("ishopdb");
-        dbo.collection("tblregister").find({}).toArray(function (err, documents) {
-            if (!err) {
-                res.send(documents);
-            } else {
-                console.log(err);
-            }
-        })
-    })
-})
+  mongoClient.connect(url, function (err, db) {
+    var dbo = db.db("ishopdb");
+    dbo
+      .collection("tblregister")
+      .find({})
+      .toArray(function (err, documents) {
+        if (!err) {
+          res.send(documents);
+        } else {
+          console.log(err);
+        }
+      });
+  });
+});
 app.get("/productDetails", function (req, res) {
-    mongoClient.connect(url, function (err, db) {
-        var dbo = db.db("ishopdb");
-        dbo.collection("tblproducts").find({}).toArray(function (err, documents) {
-            if (!err) {
-                res.send(documents);
-            } else {
-                console.log(err);
-            }
-        })
-    })
-})
+  mongoClient.connect(url, function (err, db) {
+    var dbo = db.db("ishopdb");
+    dbo
+      .collection("tblproducts")
+      .find({})
+      .toArray(function (err, documents) {
+        if (!err) {
+          res.send(documents);
+        } else {
+          console.log(err);
+        }
+      });
+  });
+});
 app.get("/categoryDetails", function (req, res) {
-    mongoClient.connect(url, function (err, db) {
-        var dbo = db.db("ishopdb");
-        dbo.collection("tblcategories").find({}).toArray(function (err, documents) {
-            if (!err) {
-                res.send(documents);
-            } else {
-                console.log(err);
-            }
-        })
-    })
-})
+  mongoClient.connect(url, function (err, db) {
+    var dbo = db.db("ishopdb");
+    dbo
+      .collection("tblcategories")
+      .find({})
+      .toArray(function (err, documents) {
+        if (!err) {
+          res.send(documents);
+        } else {
+          console.log(err);
+        }
+      });
+  });
+});
 
-
-app.listen(8080);
+app.listen(8081);
 console.log("Server Started");
